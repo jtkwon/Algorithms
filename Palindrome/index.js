2627649f0a5c6fee0bf849eaba18b52017d9f083
unknown <taerakwon@gmail.com>
2019-07-13 20:33:18 -0400

    Palindrome

---
+++ Palindrome/index.js
@@ -0,0 +1,14 @@
+/* Palindromes are strings that form same words when revered.
+Return true if Palindrome
+*/
+
+const palindrome1 = str =>
+  str ===
+  str
+    .split("")
+    .reverse()
+    .join("");
+
+const palindrome2 = str => str.split("").every((val) => )
+
+exports.palindrome1 = palindrome1;
